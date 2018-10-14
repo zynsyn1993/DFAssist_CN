6db22ae9efc5ffac8dc9850dfc0e62fb25601d6d
呆萌 <admin@diemoe.net>
2018-10-14 20:35:30 +0800

    初始化Memory类，为以后准备。

---
+++ App/Memory/Memory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal partial class Memory
+    {
+        private string gameLanguage;
+        internal bool IsRunning { get; private set; } = false;
+
+        internal Memory(string language)
+        {
+            SetLanguague(language);
+        }
+        
+        internal void StartCapture(Process process)
+        {
+        }
+
+        internal void SetLanguague(string language)
+        {
+            switch (language)
+            {
+                case "ko-kr":
+                    gameLanguage = "Korean";
+                    break;
+
+                case "en-us":
+                    gameLanguage = "English";
+                    break;
+
+                case "fr-fr":
+                    gameLanguage = "French";
+                    break;
+
+                case "ja-jp":
+                    gameLanguage = "Japanese";
+                    break;
+
+                case "zh-cn":
+                    gameLanguage = "Chinese";
+                    break;
+
+                default:
+                    return;
+            }
+        }
+    }
+}
